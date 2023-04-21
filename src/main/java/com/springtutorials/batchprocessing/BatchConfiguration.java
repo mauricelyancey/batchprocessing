350c66bb55d30a80eae6857cae4b6df5ed9b63e4
Maurice Yancey <mauricelyancey@gmail.com>
2023-04-21 18:44:28 -0400

    Initial commit

---
+++ src/main/java/com/springtutorials/batchprocessing/BatchConfiguration.java
@@ -0,0 +1,43 @@
+package com.springtutorials.batchprocessing;
+
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BatchConfiguration {
+    @Bean
+    public FlatFileItemReader<Person> reader(){
+        return new FlatFileItemReaderBuilder<Person>()
+                .name("personItemReader")
+                .resource(new ClassPathResource("sample-data.csv"))
+                .delimited()
+                .names(new String[]{"firstName", "lastName"})
+                .fieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
+                    setTargetType(Person.class);
+                }})
+                .build();
+    }
+
+    @Bean
+    public PersonItemProcessor processor(){
+        return new PersonItemProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Person> writer(DataSource dataSource){
+        return new JdbcBatchItemWriterBuilder<Person>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)")
+                .dataSource(dataSource)
+                .build();
+    }
+}
---
+++ src/test/java/com/springtutorials/batchprocessing/BatchprocessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtutorials.batchprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchprocessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
